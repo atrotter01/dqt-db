f0f0149494200b1db9785fe3c085a70dcf1068f5
Adam <adam@Alienware>
2023-07-15 21:26:59 -0400

    Add script to flush data from Redis.

---
+++ reset_redis_data.py
@@ -0,0 +1,6 @@
+from app.util import Util
+
+util = Util()
+#util.reset_processed_data('ArenaGhostNPC')
+util.clear_caches()
+util.cache_api_tables()
--- view_data.py
+++ view_data.py
@@ -1,13 +1,23 @@
+import sys
+import json
+import zlib
 from app.util import Util
 from pprint import pprint
 
 util = Util()
-asset_list: list = util.get_asset_list(asset_type='LootItemGroup')
+asset_list: list = util.get_asset_list(asset_type='Stage')
 
 for path in asset_list:
     asset = util.get_asset_by_path(path)
     path: int = asset.get('path')
-    document = asset.get('document')
+    document = asset.get('processed_document')
 
-    print(document)
-    print(document.get('displayName'))
+    #print(document)
+    #print(document.get('displayName'))
+    print('---------------------')
+    print(path)
+    print('---------------------')
+    print(sys.getsizeof(str(document)))
+    print(sys.getsizeof(zlib.compress(json.dumps(document).encode())))
+    print(sys.getsizeof(zlib.compress(json.dumps(document).encode(), level=9)))
+    print('')
