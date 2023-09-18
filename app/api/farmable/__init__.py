fbab2ce30972ec29ab2927df6dc339c1b58a09dc
Adam <adam@Alienware>
2023-09-18 17:03:13 -0400

    Add filters to farmable page

--- app/api/farmable/__init__.py
+++ app/api/farmable/__init__.py
@@ -15,9 +15,14 @@ farmable_model = api.model('farmable', {
     'enemy_id': fields.String,
     'enemy_display_name': fields.String,
     'enemy_icon': fields.String,
+    'enemy_family': fields.String,
+    'enemy_family_icon': fields.String,
+    'enemy_role': fields.String,
+    'enemy_role_icon': fields.String,
     'scout_probability': fields.Float,
     'stamina_per_drop': fields.Float,
     'stamina_per_drop_double_drop_rate': fields.Float,
+    'is_best_drop_rate': fields.Boolean
 })
 
 @api.route("/")
@@ -42,6 +47,7 @@ class Asset(Resource):
             return cached_asset
 
         asset_list: list = self.util.get_asset_list('Stage')
+        best_drop_rate_per_unit: dict = {}
 
         for path in asset_list:
             stage = self.stage_parser.get_data(path)
@@ -57,6 +63,10 @@ class Asset(Resource):
                 enemy_id: str = enemy.get('monster').get('id')
                 enemy_display_name: str = enemy.get('monster').get('enemy_display_name')
                 enemy_icon: str = enemy.get('monster').get('enemy_unit_icon')
+                enemy_family: str = enemy.get('monster').get('enemy_family')
+                enemy_family_icon: str = enemy.get('monster').get('enemy_family_icon')
+                enemy_role: str = enemy.get('monster').get('enemy_role')
+                enemy_role_icon: str = enemy.get('monster').get('enemy_role_icon')
                 scout_probability: float = float(enemy.get('monster').get('enemy_scout_probability'))
 
                 unit_key: str = f'{stage_id}_{enemy_id}'
@@ -67,6 +77,15 @@ class Asset(Resource):
                 seen_units.append(unit_key)
 
                 if scout_probability > 0:
+                    stamina_per_drop: float = round((100 / scout_probability) * stage_stamina_cost, 2)
+                    stamina_per_double_drop_rate: float = round((100 / (scout_probability * 2)) * stage_stamina_cost, 2)
+
+                    if best_drop_rate_per_unit.get(enemy_display_name) is None:
+                        best_drop_rate_per_unit[enemy_display_name] = stamina_per_drop
+
+                    if stamina_per_drop < best_drop_rate_per_unit[enemy_display_name]:
+                        best_drop_rate_per_unit[enemy_display_name] = stamina_per_drop
+
                     self.farmables.append({
                         'stage_id': stage_id,
                         'stage_area_id': stage_area_id,
@@ -77,15 +96,23 @@ class Asset(Resource):
                         'enemy_id': enemy_id,
                         'enemy_display_name': enemy_display_name,
                         'enemy_icon': enemy_icon,
+                        'enemy_family': enemy_family,
+                        'enemy_family_icon': enemy_family_icon,
+                        'enemy_role': enemy_role,
+                        'enemy_role_icon': enemy_role_icon,
                         'scout_probability': scout_probability,
-                        'stamina_per_drop': round((100 / scout_probability) * stage_stamina_cost, 2),
-                        'stamina_per_drop_double_drop_rate': round((100 / (scout_probability * 2)) * stage_stamina_cost, 2),
+                        'stamina_per_drop': stamina_per_drop,
+                        'stamina_per_drop_double_drop_rate': stamina_per_double_drop_rate,
                     })
 
             for enemy in stage.get('stage_random_enemies'):
                 enemy_id: str = enemy.get('monster').get('id')
                 enemy_display_name: str = enemy.get('monster').get('enemy_display_name')
                 enemy_icon: str = enemy.get('monster').get('enemy_unit_icon')
+                enemy_family: str = enemy.get('monster').get('enemy_family')
+                enemy_family_icon: str = enemy.get('monster').get('enemy_family_icon')
+                enemy_role: str = enemy.get('monster').get('enemy_role')
+                enemy_role_icon: str = enemy.get('monster').get('enemy_role_icon')
                 scout_probability: float = float(enemy.get('monster').get('enemy_scout_probability'))
 
                 unit_key: str = f'{stage_id}_{enemy_id}'
@@ -96,6 +123,15 @@ class Asset(Resource):
                 seen_units.append(unit_key)
 
                 if scout_probability > 0:
+                    stamina_per_drop: float = round((100 / scout_probability) * stage_stamina_cost, 2)
+                    stamina_per_double_drop_rate: float = round((100 / (scout_probability * 2)) * stage_stamina_cost, 2)
+
+                    if best_drop_rate_per_unit.get(enemy_display_name) is None:
+                        best_drop_rate_per_unit[enemy_display_name] = stamina_per_drop
+
+                    if stamina_per_drop < best_drop_rate_per_unit[enemy_display_name]:
+                        best_drop_rate_per_unit[enemy_display_name] = stamina_per_drop
+
                     self.farmables.append({
                         'stage_id': stage_id,
                         'stage_area_id': stage_area_id,
@@ -106,15 +142,23 @@ class Asset(Resource):
                         'enemy_id': enemy_id,
                         'enemy_display_name': enemy_display_name,
                         'enemy_icon': enemy_icon,
+                        'enemy_family': enemy_family,
+                        'enemy_family_icon': enemy_family_icon,
+                        'enemy_role': enemy_role,
+                        'enemy_role_icon': enemy_role_icon,
                         'scout_probability': scout_probability,
-                        'stamina_per_drop': round((100 / scout_probability) * stage_stamina_cost, 2),
-                        'stamina_per_drop_double_drop_rate': round((100 / (scout_probability * 2)) * stage_stamina_cost, 2),
+                        'stamina_per_drop': stamina_per_drop,
+                        'stamina_per_drop_double_drop_rate': stamina_per_double_drop_rate,
                     })
 
             for enemy in stage.get('stage_reinforcement_enemies'):
                 enemy_id: str = enemy.get('monster').get('id')
                 enemy_display_name: str = enemy.get('monster').get('enemy_display_name')
                 enemy_icon: str = enemy.get('monster').get('enemy_unit_icon')
+                enemy_family: str = enemy.get('monster').get('enemy_family')
+                enemy_family_icon: str = enemy.get('monster').get('enemy_family_icon')
+                enemy_role: str = enemy.get('monster').get('enemy_role')
+                enemy_role_icon: str = enemy.get('monster').get('enemy_role_icon')
                 scout_probability: float = float(enemy.get('monster').get('enemy_scout_probability'))
 
                 unit_key: str = f'{stage_id}_{enemy_id}'
@@ -125,6 +169,15 @@ class Asset(Resource):
                 seen_units.append(unit_key)
 
                 if scout_probability > 0:
+                    stamina_per_drop: float = round((100 / scout_probability) * stage_stamina_cost, 2)
+                    stamina_per_double_drop_rate: float = round((100 / (scout_probability * 2)) * stage_stamina_cost, 2)
+
+                    if best_drop_rate_per_unit.get(enemy_display_name) is None:
+                        best_drop_rate_per_unit[enemy_display_name] = stamina_per_drop
+
+                    if stamina_per_drop < best_drop_rate_per_unit[enemy_display_name]:
+                        best_drop_rate_per_unit[enemy_display_name] = stamina_per_drop
+
                     self.farmables.append({
                         'stage_id': stage_id,
                         'stage_area_id': stage_area_id,
@@ -135,10 +188,23 @@ class Asset(Resource):
                         'enemy_id': enemy_id,
                         'enemy_display_name': enemy_display_name,
                         'enemy_icon': enemy_icon,
+                        'enemy_family': enemy_family,
+                        'enemy_family_icon': enemy_family_icon,
+                        'enemy_role': enemy_role,
+                        'enemy_role_icon': enemy_role_icon,
                         'scout_probability': scout_probability,
-                        'stamina_per_drop': round((100 / scout_probability) * stage_stamina_cost, 2),
-                        'stamina_per_drop_double_drop_rate': round((100 / (scout_probability * 2)) * stage_stamina_cost, 2),
+                        'stamina_per_drop': stamina_per_drop,
+                        'stamina_per_drop_double_drop_rate': stamina_per_double_drop_rate,
                     })
+
+        for farmable in self.farmables:
+            enemy_display_name = farmable.get('enemy_display_name')
+            stamina_per_drop = farmable.get('stamina_per_drop')
+
+            if stamina_per_drop == best_drop_rate_per_unit[enemy_display_name]:
+                farmable.update({ 'is_best_drop_rate': True })
+            else:
+                farmable.update({ 'is_best_drop_rate': False })
 
         self.util.save_redis_asset(cache_key=self.cache_key, data=sorted(self.farmables, key=lambda d: d['enemy_display_name']))
 
