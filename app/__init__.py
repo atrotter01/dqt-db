ccca13c18e6b1a5533b98f289c3112981e7404b7
Adam <adam@Alienware>
2023-10-28 19:53:48 -0400

    Add master rank toggles to unit detail page

--- app/__init__.py
+++ app/__init__.py
@@ -223,8 +223,11 @@ def package_route():
 
 @app.route('/unit/<unit_id>')
 def unit_detail_route(unit_id):
+    util: Util = Util(lang=session['lang'])
     awakening: str = request.args.get('awakening') or '0'
     master_rank: str = request.args.get('master_rank') or '0'
+    stat_increase_table = util.get_asset_by_path(path='-5945447399125289372', deflate_data=True, build_processed_asset=True)
+    stat_increase_table_document = stat_increase_table.get('processed_document')
 
     api_response = requests.get(url=f'http://localhost:5000/api/unit/{unit_id}?awakening={awakening}&master_rank={master_rank}', timeout=300, params=dict(lang=session['lang']))
     farmable_api_response = requests.get(url='http://localhost:5000/api/farmable/', timeout=300, params=dict(lang=session['lang']))
@@ -251,7 +254,7 @@ def unit_detail_route(unit_id):
                     battleroads.append(area)
                     break
 
-    return render_template('unit_detail.html', unit=unit[0], farmables=farmables, battleroads=battleroads)
+    return render_template('unit_detail.html', unit=unit[0], farmables=farmables, battleroads=battleroads, awakening=awakening, master_rank=master_rank, unit_id=unit_id, stat_increase_table=stat_increase_table_document.get('items'))
 
 @app.route('/equipment/')
 def equipment_route():
